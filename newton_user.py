65edf819c5b7338252a23c2c92760791bebfea1c
M. L. Pace <76403900+mell-o-tron@users.noreply.github.com>
2022-05-08 16:59:55 +0200

    Add files via upload

--- newton_user.py
+++ newton_user.py
@@ -1,18 +1,11 @@
 from newton import runPG
-
-import pygame
-from time import sleep
-import numpy as np
 import math
-from pygame.locals import *
-import random
-import itertools
 
 
-f = lambda x : x**2 * math.exp(x) * 3 - 1
-f1 = lambda x : 6* x * math.exp(x) + 3 * x**2 * math.exp(x)
-stop = .001
-x0 = -6
-scale = 30
+f = lambda x : math.exp(-x) + x - 2
+f1 = lambda x : -math.exp(-x) + 1
+stop = .0001
+x0 = -3
+scale = 20
 
 runPG(f, f1, x0, scale, stop)
